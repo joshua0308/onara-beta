7e61299447aab87901daaec0024c914a78d788f3
Joshua Kim <joshua940308@gmail.com>
2021-01-14 00:56:13 -0500

    add socket.io to server

--- app.js
+++ app.js
@@ -1,17 +1,28 @@
-const { response } = require("express");
 const express = require('express');
 
 const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+
 const PORT = 8080;
 
-app.use(express.static(__dirname + '/build'));
+io.on('connection', socket => {
+  console.log('a user connected: ', socket.id)
+
+  socket.on('disconnect', () => {
+    console.log('user disconnected: ', socket.id)
+    io.emit('disconnect', socket.id)
+  })
+})
 
 app.get('/', (req, res) => {
   res.send('hi')
 })
 
 app.get('/game', (req, res) => {
-  res.sendFile(__dirname + '/build/index.html');
+  res.sendFile(__dirname + '/index.html');
 })
+
+app.use(express.static(__dirname + '/src'));
 
 app.listen(PORT, () => console.log(`listening on port ${PORT}...`))
