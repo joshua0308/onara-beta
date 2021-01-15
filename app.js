727b676139ce90608b5a9efdaa03dfacd37d467c
Joshua Kim <joshua940308@gmail.com>
2021-01-15 18:48:14 -0500

    use process.env.PORT

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 const players = {};
 
