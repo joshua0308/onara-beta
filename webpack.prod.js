bcaad349eb0641e26bc7a139a304d039af26df03
Joshua Kim <joshua940308@gmail.com>
2021-02-14 02:08:22 -0500

    add webpack.prod.js

---
+++ webpack.prod.js
@@ -0,0 +1,38 @@
+const { merge } = require('webpack-merge');
+const common = require('./webpack.common.js');
+const TerserPlugin = require('terser-webpack-plugin');
+
+module.exports = merge(common, {
+  devtool: 'source-map',
+  performance: {
+    hints: 'warning'
+  },
+  output: {
+    pathinfo: false
+  },
+  optimization: {
+    namedModules: false,
+    namedChunks: false,
+    nodeEnv: 'production',
+    flagIncludedChunks: true,
+    occurrenceOrder: true,
+    concatenateModules: true,
+    splitChunks: {
+      hidePathInfo: true,
+      minSize: 30000,
+      maxAsyncRequests: 5,
+      maxInitialRequests: 3
+    },
+    noEmitOnErrors: true,
+    checkWasmTypes: true,
+    minimize: true,
+    minimizer: [
+      new TerserPlugin({
+        terserOptions: {
+          keep_classnames: true,
+          keep_fnames: true
+        }
+      })
+    ]
+  }
+});
